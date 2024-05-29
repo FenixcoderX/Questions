7744b06ab336d011c7f2b25b510f9f818a78f6c8
FenixcoderX <fenixcoder2024@gmail.com>
2024-05-29 21:13:46 +0700

    Add pagination and url params to the question list

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,11 +1,45 @@
 import './QuestionList.css';
 import { connect } from 'react-redux';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 import QuestionCard from './QuestionCard';
+import Pagination from './Pagination';
 
 const QuestionList = ({ authedUser, questions }) => {
+  // Create state variable to keep track of the current page
+  const [currentPage, setCurrentPage] = useState(null);
+  console.log('currentPage: ', currentPage);
+  const [questionsPerPage] = useState(3);
+
+  // Get the current location and navigate function from the router
+  const location = useLocation();
+  const navigate = useNavigate();
+
   // Create state variable to toggle between new and answered questions
-  const [showNewQuestions, setShowNewQuestions] = useState(true);
+  const [showNewQuestions, setShowNewQuestions] = useState(null);
+
+  const handleQuestionToggle = () => {
+    setShowNewQuestions(!showNewQuestions);
+    const urlParams = new URLSearchParams(location.search);
+    console.log('showNewQuestions: ', !showNewQuestions);
+    urlParams.set('showNewQuestions', String(!showNewQuestions));
+    urlParams.set('page', '1');
+    const searchQuery = urlParams.toString();
+    navigate(`?${searchQuery}`);
+  };
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const showNewQuestions = urlParams.get('showNewQuestions');
+    console.log(showNewQuestions);
+    console.log('typeof showNewQuestions: ', typeof showNewQuestions);
+    const currentPage = urlParams.get('page');
+    //console.log(currentPage);
+    showNewQuestions
+      ? setShowNewQuestions(showNewQuestions === 'true')
+      : setShowNewQuestions(true);
+    currentPage ? setCurrentPage(Number(currentPage)) : setCurrentPage(1);
+  }, [location.search]);
 
   // Create an array with keys from the questions object (aka id). First, we filter questions
   // that the logged-in user has not responded to, and then sort them by creation time in descending order
@@ -29,6 +63,28 @@ const QuestionList = ({ authedUser, questions }) => {
     })
     .sort((a, b) => questions[b].timestamp - questions[a].timestamp);
 
+  // Get current items
+  const indexOfLastQuestion = currentPage * questionsPerPage;
+  const indexOfFirstQuestion = indexOfLastQuestion - questionsPerPage;
+  const currentNewQuestions = newQuestions.slice(
+    indexOfFirstQuestion,
+    indexOfLastQuestion
+  );
+  const currentAnsweredQuestions = answeredQuestions.slice(
+    indexOfFirstQuestion,
+    indexOfLastQuestion
+  );
+
+  // Change page
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
+    const urlParams = new URLSearchParams(location.search);
+    urlParams.set('page', String(pageNumber));
+    urlParams.set('showNewQuestions', String(showNewQuestions));
+    const searchQuery = urlParams.toString();
+    navigate(`?${searchQuery}`);
+  };
+
   return (
     <div>
       {/* Create this html code only if showNewQuestions is true */}
@@ -39,16 +95,20 @@ const QuestionList = ({ authedUser, questions }) => {
           <button
             data-testid="create-button"
             className="btn btn-outline-secondary text-nowrap question-toggle"
-            onClick={() => setShowNewQuestions(!showNewQuestions)}
+            onClick={handleQuestionToggle}
           >
             <span className="toggle-unactive">Answered</span>{' '}
             <span className="toggle-active">New</span>
           </button>
-
+          <Pagination
+            itemsPerPage={questionsPerPage}
+            totalItems={newQuestions.length}
+            paginate={paginate}
+          />
           <div className="question-list-cards">
             <ul>
               {/* Use map method to create html code for each question card using the QuestionCard component (Pass the id as a prop) */}
-              {newQuestions.map((id) => (
+              {currentNewQuestions.map((id) => (
                 <li key={id}>
                   <QuestionCard id={id} />
                 </li>
@@ -66,16 +126,20 @@ const QuestionList = ({ authedUser, questions }) => {
           <button
             data-testid="create-button"
             className="btn btn-outline-secondary text-nowrap question-toggle"
-            onClick={() => setShowNewQuestions(!showNewQuestions)}
+            onClick={handleQuestionToggle}
           >
             <span className="toggle-active">Answered</span>{' '}
             <span className="toggle-unactive">New</span>
           </button>
-
+          <Pagination
+            itemsPerPage={questionsPerPage}
+            totalItems={answeredQuestions.length}
+            paginate={paginate}
+          />
           <div className="question-list-cards">
             <ul>
               {/* Use map method to create html code for each question card using the QuestionCard component (Pass the id as a prop) */}
-              {answeredQuestions.map((id) => (
+              {currentAnsweredQuestions.map((id) => (
                 <li key={id}>
                   <QuestionCard id={id} />
                 </li>
